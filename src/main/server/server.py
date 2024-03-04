008e7d86b96c9c3b35f4903ae5e33324cd45e293
Pedro Henrick <pedrohenrickqa@gmail.com>
2024-03-04 08:54:35 -0300

    fix: pasta de templates

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -5,7 +7,7 @@ from flask_migrate import Migrate
 from ..routes import bolsistas_routes_bp
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=Path('./src/templates').absolute())
 app.register_blueprint(bolsistas_routes_bp)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
