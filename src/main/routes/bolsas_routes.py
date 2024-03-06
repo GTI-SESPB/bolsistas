c328a85f4c758f565d9f8d62fb9b7ab4d0933f87
Pedro Henrick <pedrohenrickqa@gmail.com>
2024-03-06 09:26:04 -0300

    feat: recurso de adicionar e visualizar bolsa finalizado

--- src/main/routes/bolsas_routes.py
+++ src/main/routes/bolsas_routes.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 from flask.views import MethodView
 from sqlalchemy import select
 
 from ..database import db
-from ..models import Bolsa
+from ..models import Bolsa, Edital
 from ..utils import class_route
 
 
@@ -16,7 +16,24 @@ class Listar(MethodView):
         bolsas = db.session.execute(select(Bolsa)).scalars()
         return render_template('bolsas/listar.html', bolsas=bolsas)
 
+
 @class_route(bolsas_bp, '/bolsas/adicionar', 'adicionar')
 class Adicionar(MethodView):
     def get(self):
-        return render_template('bolsas/adicionar.html')
+        editais = db.session.execute(select(Edital)).scalars()
+        print(editais)
+        return render_template('bolsas/adicionar.html', editais=editais)
+
+    def post(self):
+        bolsa = Bolsa(**request.form)
+        db.session.add(bolsa)
+        db.session.commit()
+        return redirect(url_for('bolsas.listar'))
+
+@class_route(bolsas_bp, '/bolsas/visualizar/<int:id>', 'visualizar')
+class visualizar(MethodView):
+    def get(self, id: int):
+        bolsa = db.session.execute(
+            select(Bolsa).where(Bolsa.id == id)
+        ).scalar()
+        return render_template('bolsas/visualizar.html', bolsa=bolsa)
