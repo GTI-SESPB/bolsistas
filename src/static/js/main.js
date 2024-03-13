3907d35f7fc0d19f2e79254812eee0ee4bc1d791
Pedro Henrick <pedrohenrickqa@gmail.com>
2024-03-13 11:43:16 -0300

    chore: nomes em uppercase

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -3,6 +3,11 @@ const apenasNumeros = (value) => {
   return value.replace(/\D/g, '');
 }
 
+const apenasMaiusculas = (e) => {
+  let input = e.target;
+  input.value = input.value.toUpperCase();
+}
+
 const limitarANumeros = (e) => {
   let input = e.target;
   input.value = apenasNumeros(input.value)
@@ -11,3 +16,4 @@ const limitarANumeros = (e) => {
 function redirecionar(url) {
   window.location = url
 }
+
